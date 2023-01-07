f352208e60af7ee75a7c05dbf3026ce386fc4b47
yuval4 <didi19289@gmail.com>
2023-01-07 15:39:28 +0200

    db

--- db-config.js
+++ db-config.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 const config = {
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "9289",
-  database: "blog",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.BD_DATABASE,
+  ssl: process.env.BD_SSL,
 };
 
 module.exports = config;
